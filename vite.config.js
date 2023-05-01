18edb1c82bb2886a67ab7c1ea3bbdce6d6bcab6e
Dante Vicenzo <dantevcz@gmail.com>
2023-05-01 06:59:45 -0300

    Cleanup boilerplate

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  watch: {
-    usePolling: true,
-  },
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  },
 })

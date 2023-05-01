04522f68b1f45709385f6c3aba917673d4df3d9c
Dante Vicenzo <dantevcz@gmail.com>
2023-05-01 07:21:21 -0300

    theme stated

---
+++ src/theme/theme.d.ts
@@ -0,0 +1,21 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors: {
+            white: '#FFFFFF',
+            pink: '#E4105D',
+            purple: '#1E192C',
+            grey: '#565656',
+            yellow: '#E5E044',
+            green: '#23DD7A',
+            charcoal: '#3B4651'
+        },
+        fontSizes: {
+            default: '18px',
+            h1: '32px',
+            p: '16px',
+            small: '12px',
+        }
+    }
+}
---
+++ src/theme/theme.jsx
@@ -0,0 +1,19 @@
+const theme = {
+    colors: {
+        white: '#FFFFFF',
+        pink: '#E4105D',
+        purple: '#1E192C',
+        grey: '#565656',
+        yellow: '#E5E044',
+        green: '#23DD7A',
+        charcoal: '#3B4651'
+    },
+    fontSizes: {
+        default: '18px',
+        h1: '32px',
+        p: '16px',
+        small: '12px',
+    }
+}
+
+export default theme;

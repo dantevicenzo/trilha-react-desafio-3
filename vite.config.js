91d27c891dd7d288b060dcb15df458435be22f12
Dante Vicenzo <dantevcz@gmail.com>
2023-05-02 10:43:44 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/trilha-react-desafio-3/",
   plugins: [react()],
   server: {
     watch: {
